394dc151ddaa480a93913f065ec00dd94a502340
Nyako Nyako <artur_akim@ukr.net>
2017-07-27 03:21:57 +0300

    0.1 [fix: global]

--- application/views/admin/IncomesView.php
+++ application/views/admin/IncomesView.php
@@ -1,18 +1,13 @@
 <!doctype html>
-<html lang="ru" ng-app="Firegular" ng-controller="FiregularController">
+<html lang="ru" ng-app="Amity" ng-controller="AmityController">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="Cache-Control" content="no-cache">
     <title></title>
     <link rel="icon" href="/favicon.png">
-    <link rel="stylesheet" href="/static/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/static/css/Pe-icon-7-stroke.css">
-    <link rel="stylesheet" href="http://carlosroso.com/notyf/notyf.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700&amp;subset=cyrillic">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=PT+Sans:400,700&amp;subset=cyrillic,cyrillic-ext,latin-ext">
-    <link rel="stylesheet" href="/static/css/datepicker.css">
-    <link rel="stylesheet" href="/static/css/app.css">
-    <link rel="stylesheet" href="/static/css/transactions.css">
+    <link rel="stylesheet" href="/static/css/bundle.min.css">
 </head>
 <body ng-controller="IncomesController" ng-init="initSystem()">
     <div class="loader">
@@ -21,8 +16,8 @@
     <div class="wrapper">
         <ul class="sidebar" ng-include="_templates.sidebar"></ul><!-- ./ sidebar -->
         <div class="top-panel">
-            <h4>Firegular: доходы</h4>
-            <button class="btn add-transaction" data-toggle="modal" data-target=".add-income-modal">Добавить доход</button>
+            <h4>{{title}}: доходы</h4>
+            <button class="btn add" data-toggle="modal" data-target=".add-income-modal">+ Добавить доход</button>
             <div class="user">
                 <i class="pe-7s-like" data-toggle="modal" data-target=".profile-settings-modal"></i>
                 <span>Привет, <? echo $this->session->userdata("username") ?>!</span>
@@ -30,7 +25,7 @@
             </div>
         </div><!-- ./ top-panel -->
         <main class="main">
-            <div class="container-fluid">
+            <div class="container-fluid" style="padding: 0;">
                 <table class="table table-striped">
                     <thead>
                         <tr>
@@ -99,7 +94,7 @@
                             <input type="text" class="form-control datepicker" placeholder="Дата" ng-model="income_date" required>
                         </div>
                         <button class="btn cancel-button" data-dismiss="modal">Отмена</button>
-                        <button class="btn save-button" type="submit">Добавить</button>
+                        <button class="btn add" type="submit">Добавить</button>
                     </form>
                 </div><!-- ./ modal-body -->
             </div>
@@ -108,16 +103,8 @@
     <!-- ./ add income modal -->
     <!-- profile settings modal -->
     <div class="modal fade profile-settings-modal" ng-include="_templates.profileSettingsModal"></div>
-    <!-- ./ profile settings model -->
-    <script src="/static/js/libs/angular.min.js"></script>
-    <script src="/static/js/libs/jquery.min.js"></script>
-    <script src="/static/js/libs/tether.min.js"></script>
-    <script src="/static/js/libs/bootstrap.min.js"></script>
-    <script src="/static/js/libs/notyf.min.js"></script>
-    <script src="/static/js/libs/underscore.min.js"></script>
-    <script src="/static/js/libs/moment.min.js"></script>
-    <script src="/static/js/libs/moment-datepicker.js"></script>
-    <script src="/static/js/libs/ru.js"></script>
+    <!-- ./ profile settings modal -->
+    <script src="/static/js/bundle.min.js"></script>
     <script src="/static/js/modules/admin.js"></script>
 </body>
 </html>
