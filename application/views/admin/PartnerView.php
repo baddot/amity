3e0198ec33a38998c1855d97aa930205710bf754
Nyako Nyako <artur_akim@ukr.net>
2017-07-27 03:22:44 +0300

    0.1 [fix: global]

--- application/views/admin/PartnerView.php
+++ application/views/admin/PartnerView.php
@@ -1,17 +1,13 @@
 <!doctype html>
-<html lang="ru" ng-app="Firegular" ng-controller="FiregularController">
+<html lang="ru" ng-app="Amity" ng-controller="AmityController">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="Cache-Control" content="no-cache">
     <title></title>
     <link rel="icon" href="/favicon.png">
-    <link rel="stylesheet" href="/static/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/static/css/Pe-icon-7-stroke.css">
-    <link rel="stylesheet" href="http://carlosroso.com/notyf/notyf.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700&amp;subset=cyrillic">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=PT+Sans:400,700&amp;subset=cyrillic,cyrillic-ext,latin-ext">
-    <link rel="stylesheet" href="/static/css/app.css">
-    <link rel="stylesheet" href="/static/css/partner.css">
+    <link rel="stylesheet" href="/static/css/bundle.min.css">
 </head>
 <body ng-controller="PartnerController" ng-init="initSystem()">
     <div class="loader">
@@ -20,8 +16,8 @@
     <div class="wrapper">
         <ul class="sidebar" ng-include="_templates.sidebar"></ul><!-- ./ sidebar -->
         <div class="top-panel">
-            <h4>Firegular: партнеры</h4>
-            <button class="btn add-partner" data-toggle="modal" data-target=".add-partner-modal">Добавить партнера</button>
+            <h4>{{title}}: партнеры</h4>
+            <button class="btn add" data-toggle="modal" data-target=".add-partner-modal">+ Добавить партнера</button>
             <div class="user">
                 <i class="pe-7s-like" data-toggle="modal" data-target=".profile-settings-modal"></i>
                 <span>Привет, <? echo $this->session->userdata("username") ?>!</span>
@@ -32,7 +28,7 @@
             <div class="container-fluid">
                 <div class="row">
                     <div class="partner-handlers">
-                        <input class="search" placeholder="Поиск" ng-model="">
+                        <input class="search" placeholder="Поиск" ng-model="search">
                     </div>
                     <table class="table table-striped table-partners">
                         <thead>
@@ -44,19 +40,19 @@
                                 <th>Категория услуг</th>
                                 <th>Представитель</th>
                                 <th>Тел. представителя</th>
-                                <th width="5%"></th>
+                                <th width="1%"></th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr ng-repeat="partner in partners | filter: partner_name | orderBy: 'partner.partner_id'">
+                            <tr ng-repeat="partner in partners | filter: search | orderBy: 'partner.partner_id'">
                                 <td>{{partner.id}}</td>
-                                <td>{{partner.name}}</td>
+                                <td>{{partner.partner_name}}</td>
                                 <td>{{partner.phone}}</td>
                                 <td>{{partner.email}}</td>
                                 <td>{{partner.name}}</td>
                                 <td>{{partner.agent_name == "undefined" ? "Нету" : partner.agent_name}}</td>
                                 <td>{{partner.agent_phone == 0 ? "Нету" : partner.agent_phone}}</td>
-                                <td><button class="btn delete-partner" ng-click="deletePartner(partner)">Удалить</button></td>
+                                <td><i class="pe-7s-trash" ng-click="deletePartner(partner)" style="color: #000"></i></td>
                             </tr>
                         </tbody>
                     </table>
@@ -98,7 +94,7 @@
                             <input type="tel" class="form-control" ng-model="agent_phone" placeholder="Тел. представителя">
                         </div>
                         <button class="btn cancel-button" data-dismiss="modal">Отмена</button>
-                        <button class="btn save-button" type="submit">Добавить</button>
+                        <button class="btn add" type="submit">Добавить</button>
                     </form>
                 </div><!-- ./ modal-body -->
             </div>
@@ -108,13 +104,10 @@
     <!--add user modal -->
     <div class="modal fade add-user-modal" ng-include="_templates.addUserModal"></div>
     <!-- ./ add user modal -->
-    <script src="/static/js/libs/angular.min.js"></script>
-    <script src="/static/js/libs/jquery.min.js"></script>
-    <script src="/static/js/libs/tether.min.js"></script>
-    <script src="/static/js/libs/bootstrap.min.js"></script>
-    <script src="/static/js/libs/notyf.min.js"></script>
-    <script src="/static/js/libs/underscore.min.js"></script>
-    <script src="/static/js/libs/moment.min.js"></script>
+    <!-- profile settings modal -->
+    <div class="modal fade profile-settings-modal" ng-include="_templates.profileSettingsModal"></div>
+    <!-- ./ profile settings modal -->
+    <script src="/static/js/bundle.min.js"></script>
     <script src="/static/js/modules/admin.js"></script>
 </body>
 </html>
