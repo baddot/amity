f0af4a838828f45a8e379ee93e1bf9d5fac42282
Nyako Nyako <artur_akim@ukr.net>
2017-07-27 03:20:51 +0300

    0.1 [fix: global]

--- application/views/admin/BalanceView.php
+++ application/views/admin/BalanceView.php
@@ -1,17 +1,13 @@
 <!doctype html>
-<html lang="ru" ng-app="Firegular" ng-controller="FiregularController">
+<html lang="ru" ng-app="Amity" ng-controller="AmityController">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="Cache-Control" content="no-cache">
     <title></title>
     <link rel="icon" href="/favicon.png">
-    <link rel="stylesheet" href="/static/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/static/css/Pe-icon-7-stroke.css">
-    <link rel="stylesheet" href="http://carlosroso.com/notyf/notyf.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700&amp;subset=cyrillic">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=PT+Sans:400,700&amp;subset=cyrillic,cyrillic-ext,latin-ext">
-    <link rel="stylesheet" href="/static/css/app.css">
-    <link rel="stylesheet" href="/static/css/balance.css">
+    <link rel="stylesheet" href="/static/css/bundle.min.css">
 </head>
 <body ng-controller="BalanceController" ng-init="initSystem()">
     <div class="loader">
@@ -20,7 +16,7 @@
     <div class="wrapper">
         <ul class="sidebar" ng-include="_templates.sidebar"></ul><!-- ./ sidebar -->
         <div class="top-panel">
-            <h4>Firegular: баланс</h4>
+            <h4>{{title}}: баланс</h4>
             <div class="user">
                 <i class="pe-7s-like" data-toggle="modal" data-target=".profile-settings-modal"></i>
                 <span>Привет, <? echo $this->session->userdata("username") ?>!</span>
@@ -88,7 +84,7 @@
                             <input type="number" class="form-control" ng-model="sum" placeholder="Сумма" required>
                         </div>
                         <button class="btn cancel-button" data-dismiss="modal">Отмена</button>
-                        <button class="btn save-button" type="submit">Конвертировать</button>
+                        <button class="btn add" type="submit">Конвертировать</button>
                     </form>
                 </div><!-- ./ modal-body -->
             </div>
@@ -97,14 +93,8 @@
     <!-- ./ convert modal -->
     <!-- profile settings modal -->
     <div class="modal fade profile-settings-modal" ng-include="_templates.profileSettingsModal"></div>
-    <!-- ./ profile settings model -->
-    <script src="/static/js/libs/angular.min.js"></script>
-    <script src="/static/js/libs/jquery.min.js"></script>
-    <script src="/static/js/libs/tether.min.js"></script>
-    <script src="/static/js/libs/bootstrap.min.js"></script>
-    <script src="/static/js/libs/notyf.min.js"></script>
-    <script src="/static/js/libs/underscore.min.js"></script>
-    <script src="/static/js/libs/moment.min.js"></script>
+    <!-- ./ profile settings modal -->
+    <script src="/static/js/bundle.min.js"></script>
     <script src="/static/js/modules/admin.js"></script>
 </body>
 </html>
